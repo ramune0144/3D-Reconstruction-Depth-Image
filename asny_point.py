b8370e5ad011421dd0f1cd84b63d42add318a208
Hatsakon <hasakron@gmail.com>
2021-06-14 19:25:29 +0700

    fix 555

--- asny_point.py
+++ asny_point.py
@@ -9,7 +9,7 @@ import asyncio
 from itertools import chain 
 from scr import mat_con
 import math as m
-FL = 200#fl val is not defind
+FL = 200#555
 scalingFactor = 200
 def resize(image):
     img = image 
