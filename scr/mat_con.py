0065b623a8114be578140b05a59387b919ecc93c
naruepon sowchai <naruepon.sao@ku.th>
2021-06-14 17:22:18 +0700

    v_1.0

---
+++ scr/mat_con.py
@@ -0,0 +1,24 @@
+import numpy as np
+import math as m
+  
+def Rx(theta):
+  return np.matrix([[ 1, 0           , 0           ],
+                   [ 0, m.cos(theta),-m.sin(theta)],
+                   [ 0, m.sin(theta), m.cos(theta)]])
+  
+def Ry(theta):
+  return np.matrix([[ m.cos(theta), 0, m.sin(theta)],
+                   [ 0           , 1, 0           ],
+                   [-m.sin(theta), 0, m.cos(theta)]])
+  
+def Rz(theta):
+  return np.matrix([[ m.cos(theta), -m.sin(theta), 0 ],
+                   [ m.sin(theta), m.cos(theta) , 0 ],
+                   [ 0           , 0            , 1 ]])
+
+
+
+def _R(phi,theta,psi):
+    R = Rz(psi) * Ry(theta) * Rx(phi)
+    return R
+#print(np.round(_R(phi,theta,psi), decimals=2))
