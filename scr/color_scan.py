ec099c3ee9e857f7efa7b7aa0533a46d7cc26570
Rabbit101 <70846170+ramune0144@users.noreply.github.com>
2021-06-20 01:55:01 +0700

    Update color_scan.py

--- scr/color_scan.py
+++ scr/color_scan.py
@@ -14,21 +14,11 @@ def img_scan(rgb,color):
         for u in range(rgb.size[0]):# width
              if(rgb.getpixel((u,v)) == color[0]):
                 cod.append([u,v])
-                break
-                #v_point =True
-                #u_start = True
-        #if v_point == False and u_start == True: break #หากหมดเเล้วออกloopเลย
-        #v_point = False         
+                break       
     return cod[0]
-#ตัดภาพ
-
+# test #
 cod = img_scan(rgb,color)
 print(cod)
 img_c = rgb.crop((cod[0]-25,cod[1]-21,1229,516))
-#img_c =image[cod[0][1]:cod[1][1],cod[0][0]:cod[1][0]] #[start_y:end_y, start_x:end_x]
-#print(rgb.convert("I").getpixel((94,564)))
-##
-#เเสดงภาพ
-#img = Image.fromarray(img_c,'RGB')#ทำให้ np.arrayเป็นimgเเบบgrayscale
 img_c.convert("I").show()
-##
+
